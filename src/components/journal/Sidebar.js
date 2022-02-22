cad91aeac203d285ad0f68810b9140de38a4c713
Nelson Cadenas <nca1478@yahoo.com>
2022-02-22 16:49:05 -0400

    added Sidebar component

---
+++ src/components/journal/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <aside className="journal__sidebar">
+      <div className="journal__sidebar-navbar">
+        <h3 className="mt-5">
+          <i className="fa-regular fa-moon"></i>
+          <span> Nelson Cadenas</span>
+        </h3>
+        <button className="btn">Logout</button>
+      </div>
+
+      <div className="journal__new-entry">
+        <i className="fa-regular fa-calendar fa-5x"></i>
+        <p className="mt-5">New Entry</p>
+      </div>
+    </aside>
+  )
+}
