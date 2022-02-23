cb83200efd68d138a2f6a60e1b187c91223d9fc8
Nelson Cadenas <nca1478@yahoo.com>
2022-02-23 05:24:19 -0400

    added handler errors user register form

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
+import validator from 'validator'
 
 export const RegisterScreen = () => {
   const [formValues, handleInputChange] = useForm({
@@ -18,13 +19,39 @@ export const RegisterScreen = () => {
 
   const handleRegister = (e) => {
     e.preventDefault()
-    console.log(name, email, password, password2)
+
+    if (isFormValid()) {
+      console.log('Correct Form')
+    }
+  }
+
+  const isFormValid = () => {
+    if (name.trim().length === 0) {
+      console.error('Name is required')
+      return false
+    } else if (!validator.isEmail(email)) {
+      console.error('Email is not valid')
+      return false
+    } else if (password.length < 1) {
+      console.error('Password is required')
+      return false
+    } else if (password2.length < 1) {
+      console.error('Password confirm is required')
+      return false
+    } else if (password !== password2 || password.length < 5) {
+      console.error(
+        'Password should be at least 6 characters and match each other'
+      )
+      return false
+    }
+    return true
   }
 
   return (
     <>
       <h3 className="auth__title">Register</h3>
       <form onSubmit={handleRegister}>
+        <div className="auth__alert-error">Hola Mundo</div>
         <input
           type="text"
           className="auth__input"
