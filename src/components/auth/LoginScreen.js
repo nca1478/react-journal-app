829a35feb591fded1de6e1e0d544081db3951ec7
Nelson Cadenas <nca1478@yahoo.com>
2022-02-23 00:09:48 -0400

    added Thunk Middleware - Async Actions

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
-import { login } from '../actions/auth'
+import { startLoginEmailPassword } from '../actions/auth'
 
 export const LoginScreen = () => {
   const dispatch = useDispatch()
@@ -14,7 +14,7 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault()
-    dispatch(login(12345, 'Pepe'))
+    dispatch(startLoginEmailPassword(email, password))
   }
 
   return (
