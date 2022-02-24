ae26d7dbf066c0559b6c100c1200bb6cfbbd7393
Nelson Cadenas <nca1478@yahoo.com>
2022-02-24 00:44:43 -0400

    added create new note

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../actions/auth'
+import { startNewNote } from '../../actions/notes'
 import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
@@ -9,6 +10,10 @@ export const Sidebar = () => {
 
   const handleLogout = () => {
     dispatch(startLogout())
+  }
+
+  const handleAddNew = () => {
+    dispatch(startNewNote())
   }
 
   return (
@@ -23,7 +28,7 @@ export const Sidebar = () => {
         </button>
       </div>
 
-      <div className="journal__new-entry">
+      <div className="journal__new-entry" onClick={handleAddNew}>
         <i className="fa-regular fa-calendar fa-5x"></i>
         <p className="mt-5">New Entry</p>
       </div>
