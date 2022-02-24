92df1e886bf726ee8ff10da6e6cd4f3b8153f59a
Nelson Cadenas <nca1478@yahoo.com>
2022-02-24 13:11:22 -0400

    improved load notes

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from '../firebase/firebase-config'
+import { loadNotes } from '../helpers/loadNotes'
 import { types } from '../types/types'
 
 export const startNewNote = () => {
@@ -13,6 +14,14 @@ export const startNewNote = () => {
     const doc = await db.collection(`${uid}/journal/notes`).add(newNote)
 
     dispatch(activeNote(doc.id, newNote))
+  }
+}
+
+export const startLoadingNotes = (uid) => {
+  return async (dispatch) => {
+    const notes = await loadNotes(uid) // load notes from firebase (helper)
+
+    dispatch(setNotes(notes)) // set notes to store
   }
 }
 
