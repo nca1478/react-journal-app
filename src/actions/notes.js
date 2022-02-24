d3e37f6bdf0cb123c49a33284e3e4297ed21b285
Nelson Cadenas <nca1478@yahoo.com>
2022-02-24 12:35:10 -0400

    activate note created

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from '../firebase/firebase-config'
+import { types } from '../types/types'
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -10,6 +11,17 @@ export const startNewNote = () => {
     }
 
     const doc = await db.collection(`${uid}/journal/notes`).add(newNote)
-    console.log(doc)
+
+    dispatch(activeNote(doc.id, newNote))
+  }
+}
+
+export const activeNote = (id, note) => {
+  return {
+    type: types.notesActive,
+    payload: {
+      id,
+      ...note,
+    },
   }
 }
