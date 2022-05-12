b7790b88628bb3e85f7e40eddf488008a675d671
Nelson Cadenas <nca1478@yahoo.com>
2022-05-11 20:43:31 -0400

    updated: copyright footer section (JournalScreen)

--- src/components/journal/Footer.js
+++ src/components/journal/Footer.js
@@ -2,7 +2,16 @@
 export const Footer = () => {
   return (
     <div className="footer">
-      <p>Copyright &copy; 2022 JournalApp</p>
+      <p>
+        Copyright &copy; 2022 - Developed by:{' '}
+        <a
+          href="https://nelsoncadenas.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nelson Cadenas
+        </a>
+      </p>
     </div>
   )
 }
